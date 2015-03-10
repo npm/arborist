5e91322b0c25f1412823b831f6f7f98ad98e740e
Forrest L Norvell <forrest@npmjs.com>
2015-03-09 23:15:01 -0700

    src: reformat according to `standard`
    Also, 'package' is a no-no identifier in ES3, ES6, and ES5 strict mode,
    so changed it to the more nonconfrontational 'pkg'.

--- rpt.js
+++ rpt.js
@@ -1,185 +1,177 @@
-var fs = require ('fs')
-var rpj = require ('read-package-json')
-var path = require ('path')
-var dz = require ('dezalgo')
-var once = require ('once')
-var readdir = require ('readdir-scoped-modules')
-var debug = require ('debuglog') ('rpt')
+var fs = require('fs')
+var rpj = require('read-package-json')
+var path = require('path')
+var dz = require('dezalgo')
+var once = require('once')
+var readdir = require('readdir-scoped-modules')
+var debug = require('debuglog')('rpt')
 
 function dpath (p) {
-  if (p . indexOf (process.cwd()) === 0)
-    p = p . substr (process.cwd().length + 1)
+  if (p.indexOf(process.cwd()) === 0) {
+    p = p.substr(process.cwd().length + 1)
+  }
   return p
 }
 
-module . exports = rpt
+module.exports = rpt
 
-rpt . Node = Node
-rpt . Link = Link
+rpt.Node = Node
+rpt.Link = Link
 
 var ID = 0
-function Node (package, path, cache) {
-  if (cache [path])
-    return cache [path]
+function Node (pkg, path, cache) {
+  if (cache[path]) return cache[path]
 
-  if (! (this instanceof Node))
-    return new Node (package, path, cache)
+  if (!(this instanceof Node)) {
+    return new Node(pkg, path, cache)
+  }
 
-  cache [path] = this
+  cache[path] = this
 
-  debug (this . constructor . name, dpath (path), package && package . _id)
+  debug(this.constructor.name, dpath(path), pkg && pkg._id)
 
-  this . id = ID ++
-  this . package = package
-  this . path = path
-  this . realpath = path
-  this . parent = null
-  this . children = []
+  this.id = ID++
+  this.package = pkg
+  this.path = path
+  this.realpath = path
+  this.parent = null
+  this.children = []
 }
-Node . prototype . package = null
-Node . prototype . path = ''
-Node . prototype . realpath = ''
-Node . prototype . children = null
 
+Node.prototype.package = null
+Node.prototype.path = ''
+Node.prototype.realpath = ''
+Node.prototype.children = null
 
+function Link (pkg, path, realpath, cache) {
+  if (cache[path]) return cache[path]
 
-function Link (package, path, realpath, cache) {
-  if (cache [path])
-    return cache [path]
+  if (!(this instanceof Link)) {
+    return new Link(pkg, path, realpath)
+  }
 
-  if (! (this instanceof Link))
-    return new Link (package, path, realpath)
+  cache[path] = this
 
-  cache [path] = this
+  debug(this.constructor.name, dpath(path), pkg && pkg._id)
 
-  debug (this . constructor . name, dpath (path), package && package . _id)
-
-  this . id = ID ++
-  this . path = path
-  this . realpath = realpath
-  this . package = package
-  this . parent = null
-  this . target = new Node (package, realpath, cache)
-  this . children = this . target . children
+  this.id = ID++
+  this.path = path
+  this.realpath = realpath
+  this.package = pkg
+  this.parent = null
+  this.target = new Node(pkg, realpath, cache)
+  this.children = this.target.children
 }
-Link . prototype = Object . create (Node . prototype, {
-  constructor : { value : Link }
+
+Link.prototype = Object.create(Node.prototype, {
+  constructor: { value: Link }
 })
-Link . prototype . target = null
-Link . prototype . realpath = ''
-
-
+Link.prototype.target = null
+Link.prototype.realpath = ''
 
 function loadNode (p, cache, cb) {
-  debug ('loadNode', dpath (p))
-  fs . realpath (p, function (er, real) {
-    if (er)
-      return cb (er)
-    debug ('realpath p=%j real=%j', dpath (p), dpath (real))
-    var pj = path . resolve (real, 'package.json')
-    rpj (pj, function (er, package) {
-      package = package || null
+  debug('loadNode', dpath(p))
+  fs.realpath(p, function (er, real) {
+    if (er) return cb(er)
+    debug('realpath p=%j real=%j', dpath(p), dpath(real))
+    var pj = path.resolve(real, 'package.json')
+    rpj(pj, function (er, pkg) {
+      pkg = pkg || null
       var n
-      if (p === real)
-        n = new Node (package, real, cache)
-      else
-        n = new Link (package, p, real, cache)
+      if (p === real) {
+        n = new Node(pkg, real, cache)
+      } else {
+        n = new Link(pkg, p, real, cache)
+      }
 
-      cb (er, n)
+      cb(er, n)
     })
   })
 }
 
 function loadChildren (node, cache, cb) {
-  debug ('loadChildren', dpath(node . path))
+  debug('loadChildren', dpath(node.path))
   // don't let it be called more than once
-  cb = once (cb)
-  var nm = path . resolve (node . path, 'node_modules')
-  readdir (nm, function (er, kids) {
+  cb = once(cb)
+  var nm = path.resolve(node.path, 'node_modules')
+  readdir(nm, function (er, kids) {
     // If there are no children, that's fine, just return
-    if (er)
-      return cb (null, node)
+    if (er) return cb(null, node)
 
-    kids = kids . filter (function (kid) {
+    kids = kids.filter(function (kid) {
       return kid[0] !== '.'
     })
 
     var l = kids . length
-    if (l === 0)
-      return cb (null, node)
+    if (l === 0) return cb(null, node)
 
-    kids . forEach (function (kid) {
-      var p = path . resolve (nm, kid)
-      loadNode (p, cache, then)
+    kids.forEach(function (kid) {
+      var p = path.resolve(nm, kid)
+      loadNode(p, cache, then)
     })
 
     function then (er, kid) {
-      if (er)
-        return cb (er)
-      node . children . push (kid)
-      kid . parent = node
-      if (-- l === 0) {
-        sortChildren (node)
-        return cb (null, node)
+      if (er) return cb(er)
+
+      node.children.push(kid)
+      kid.parent = node
+      if (--l === 0) {
+        sortChildren(node)
+        return cb(null, node)
       }
     }
   })
 }
 
 function sortChildren (node) {
-  node . children = node . children . sort (function (a, b) {
-    a = a . package . name . toLowerCase ()
-    b = b . package . name . toLowerCase ()
+  node.children = node.children.sort(function (a, b) {
+    a = a.package.name.toLowerCase()
+    b = b.package.name.toLowerCase()
     return a > b ? 1 : -1
   })
 }
 
 function loadTree (node, did, cache, cb) {
-  debug ('loadTree', dpath (node . path), !! cache [ node . path ])
+  debug('loadTree', dpath(node.path), !!cache[node.path])
 
-  if (did [ node . realpath ]) {
-    return dz (cb) (null, node)
+  if (did[node.realpath]) {
+    return dz(cb)(null, node)
   }
 
-  did [ node . realpath ] = true
+  did[node.realpath] = true
 
-  cb = once (cb)
-  loadChildren (node, cache, function (er, node) {
-    if (er)
-      return cb (er)
+  cb = once(cb)
+  loadChildren(node, cache, function (er, node) {
+    if (er) return cb(er)
 
-    var kids = node . children . filter (function (kid) {
-      return !did [ kid . realpath ]
+    var kids = node.children.filter(function (kid) {
+      return !did[kid.realpath]
     })
 
-    var l = kids . length
-    if (l === 0)
-      return cb (null, node)
+    var l = kids.length
+    if (l === 0) return cb(null, node)
 
-    kids . forEach (function (kid, index) {
-      loadTree (kid, did, cache, then)
+    kids.forEach(function (kid, index) {
+      loadTree(kid, did, cache, then)
     })
 
     function then (er, kid) {
-      if (er)
-        return cb (er)
+      if (er) return cb(er)
 
-      if (--l === 0)
-        cb (null, node)
+      if (--l === 0) cb(null, node)
     }
   })
 }
 
 function rpt (root, cb) {
-  root = path . resolve (root)
-  debug ('rpt', dpath (root))
-  var cache = Object . create (null)
-  loadNode (root, cache, function (er, node) {
+  root = path.resolve(root)
+  debug('rpt', dpath(root))
+  var cache = Object.create(null)
+  loadNode(root, cache, function (er, node) {
     // if there's an error, it's fine, as long as we got a node
-    if (!node)
-      return cb (er)
-    loadTree (node, {}, cache, function (lter, tree) {
-      cb(er && er.code !== "ENOENT" ? er : lter, tree)
+    if (!node) return cb(er)
+    loadTree(node, {}, cache, function (lter, tree) {
+      cb(er && er.code !== 'ENOENT' ? er : lter, tree)
     })
   })
 }
--- test/basic.js
+++ test/basic.js
@@ -1,12 +1,11 @@
-var test = require ('tap') . test
-var rpt = require ('../rpt.js')
-var path = require ('path')
-var fs = require ('fs')
-var archy = require ('archy')
-var fixtures = path . resolve (__dirname, 'fixtures')
+var test = require('tap').test
+var rpt = require('../rpt.js')
+var path = require('path')
+var fs = require('fs')
+var archy = require('archy')
+var fixtures = path.resolve(__dirname, 'fixtures')
 var roots = [ 'root', 'other', 'selflink' ]
-var cwd = path . resolve (__dirname, '..')
-var fs = require ('fs')
+var cwd = path.resolve(__dirname, '..')
 
 var symlinks = {
   'selflink/node_modules/@scope/z/node_modules/glob':
@@ -16,85 +15,87 @@ var symlinks = {
 }
 
 function cleanup () {
-  Object . keys (symlinks) . forEach (function (s) {
-    var p = path . resolve (cwd, 'test/fixtures', s)
+  Object.keys(symlinks).forEach(function (s) {
+    var p = path.resolve(cwd, 'test/fixtures', s)
     try {
-      fs . unlinkSync (p)
+      fs.unlinkSync(p)
     } catch (er) {}
   })
 }
 
-test ('setup symlinks', function (t) {
-  cleanup ()
+test('setup symlinks', function (t) {
+  cleanup()
 
-  Object . keys (symlinks) . forEach (function (s) {
-    var p = path . resolve (cwd, 'test/fixtures', s)
-    fs . symlinkSync (symlinks [ s ], p, 'dir')
+  Object.keys(symlinks).forEach(function (s) {
+    var p = path.resolve(cwd, 'test/fixtures', s)
+    fs.symlinkSync(symlinks [ s ], p, 'dir')
   })
 
-  t . end ()
+  t.end()
 })
 
-roots . forEach (function (root) {
-  var dir = path . resolve (fixtures, root)
-  var out = path . resolve (dir, 'archy.txt')
+roots.forEach(function (root) {
+  var dir = path.resolve(fixtures, root)
+  var out = path.resolve(dir, 'archy.txt')
 
-  test (root, function (t) {
-    rpt (dir, function (er, d) {
-      if (er && er.code !== "ENOENT")
-        throw er
-      var actual = archy (archyize (d)) . trim ()
+  test(root, function (t) {
+    rpt(dir, function (er, d) {
+      if (er && er.code !== 'ENOENT') throw er
+
+      var actual = archy(archyize(d)).trim()
       // console . log ('----', dir)
-      console . log (actual)
+      console.log(actual)
       // console . log (require ('util') . inspect (d, {
       //   depth: Infinity
       // }))
-      var expect = fs . readFileSync (out, 'utf8') . trim ()
-      t . equal (actual, expect)
-      t . end()
+      var expect = fs.readFileSync(out, 'utf8').trim()
+      t.equal(actual, expect)
+      t.end()
     })
   })
 })
 
-test ('broken json', function (t) {
-  rpt(path . resolve (fixtures, "bad"), function (er, d) {
-    t . ok (er, "Got an error object")
-    t . equal (er && er.code, "EJSONPARSE")
-    t . ok (d, "Got a tree")
-    t . end()
+test('broken json', function (t) {
+  rpt(path.resolve(fixtures, 'bad'), function (er, d) {
+    t.ok(er, 'Got an error object')
+    t.equal(er && er.code, 'EJSONPARSE')
+    t.ok(d, 'Got a tree')
+    t.end()
   })
 })
 
 test('missing json does not obscure deeper errors', function (t) {
-  rpt(path . resolve (fixtures, "empty"), function (er, d) {
-    t . ok (er, "Got an error object")
-    t . equal (er && er.code, "EJSONPARSE")
-    t . ok (!d, "No tree on internal error")
-    t . end()
+  rpt(path.resolve(fixtures, 'empty'), function (er, d) {
+    t.ok(er, 'Got an error object')
+    t.equal(er && er.code, 'EJSONPARSE')
+    t.ok(!d, 'No tree on internal error')
+    t.end()
   })
 })
 
 function archyize (d, seen) {
   seen = seen || {}
-  var path = d . path
-  if (d . target)
-    path = d . target . path
+  var path = d.path
+  if (d.target) {
+    path = d.target.path
+  }
 
-  var label = d . package ? d . package . _id + ' ' : ''
-  label += path . substr (cwd . length + 1)
+  var label = d.package ? d.package._id + ' ' : ''
+  label += path.substr(cwd.length + 1)
 
-  if (d . target)
+  if (d . target) {
     return { label: label + ' (symlink)', nodes: [] }
+  }
 
   return {
-    label : label,
-    nodes : d . children . map (function (kid) {
-      return archyize (kid, seen)
+    label: label,
+    nodes: d.children.map(function (kid) {
+      return archyize(kid, seen)
     })
   }
 }
 
 test('cleanup', function (t) {
-  cleanup ()
-  t . end ()
+  cleanup()
+  t.end()
 })

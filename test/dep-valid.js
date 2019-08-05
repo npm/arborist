df08ef2d313239fd6cc4433f3a21713fb6dbdc53
isaacs <i@izs.me>
2019-08-05 12:53:05 -0700

    dep-valid: don't rely on package.json _fields

--- test/dep-valid.js
+++ test/dep-valid.js
@@ -5,6 +5,12 @@ const npa =  require('npm-package-arg')
 t.ok(depValid({}, '', {}), '* is always ok')
 
 t.ok(depValid({
+  package: {
+    version: '1.2.3',
+  },
+}, '1.x', {}), 'range that is satisfied')
+
+t.ok(depValid({
   isLink: true,
   realpath: '/some/path'
 }, npa('file:/some/path'), {}), 'links must point at intended target')
@@ -21,9 +27,9 @@ t.notOk(depValid({
 
 t.ok(depValid({
   name: 'foo',
+  resolved: 'git://host/repo#somebranch',
   package: {
     version: '1.2.3',
-    _from: 'git://host/repo',
   },
 }, 'git://host/repo#semver:1.x', {}), 'git url with semver range')
 
@@ -36,50 +42,53 @@ t.ok(depValid({
 }, 'npm:bar@1.2.3', {}), 'alias is ok')
 
 t.ok(depValid({
-  package: {
-    _requested: {
-      type: 'remote',
-      raw: 'https://registry/abbrev-1.1.1.tgz',
-      rawSpec: 'https://registry/abbrev-1.1.1.tgz',
-      saveSpec: 'https://registry/abbrev-1.1.1.tgz',
-      fetchSpec: 'https://registry/abbrev-1.1.1.tgz',
-    },
-  },
-}, 'https://registry/abbrev-1.1.1.tgz', {}), 'child with matching rawSpec')
+  resolved: 'https://registry/abbrev-1.1.1.tgz',
+  package: {},
+}, 'https://registry/abbrev-1.1.1.tgz', {}), 'remote url match')
 
 t.ok(depValid({
-  package: {
-    _requested: {
-      type: 'remote',
-      saveSpec: 'https://registry/abbrev-1.1.1.tgz',
-    },
-  },
-}, 'https://registry/abbrev-1.1.1.tgz', {}), 'child with matching saveSpec')
-
-t.notOk(depValid({
-  package: {
-    _requested: {
-      type: 'remote',
-      saveSpec: 'https://registry/abbrev-1.1.2.tgz',
-    },
-  },
-}, 'https://registry/abbrev-1.1.1.tgz', {}), 'child with ummatched saveSpec')
-
-
-t.ok(depValid({
-  package: {
-    _from: 'https://registry/abbrev-1.1.1.tgz',
-  },
-}, 'https://registry/abbrev-1.1.1.tgz', {}), 'matching _from spec')
-
-t.ok(depValid({
-  package: {
-    _from: 'git+ssh://git@github.com/foo/bar',
-  },
+  resolved: 'git+ssh://git@github.com/foo/bar',
+  package: {},
 }, 'git+ssh://git@github.com/foo/bar.git', {}), 'matching _from saveSpec')
 
 t.notOk(depValid({
-  package: {
-    _from: 'git+ssh://git@github.com/foo/bar',
-  },
-}, 'git+ssh://git@github.com/bar/foo.git', {}), 'nothing matched')
+  resolved: 'git+ssh://git@github.com/foo/bar',
+  package: {},
+}, 'git+ssh://git@github.com/bar/foo.git', {}), 'different repo')
+
+t.notOk(depValid({
+  package: {},
+}, 'git+ssh://git@github.com/bar/foo.git', {}), 'missing repo')
+
+t.ok(depValid({
+  resolved: '/path/to/tarball.tgz',
+}, '/path/to/tarball.tgz', {}), 'same tarball')
+
+t.notOk(depValid({
+  resolved: '/path/to/other/tarball.tgz',
+}, '/path/to/tarball.tgz', {}), 'different tarball')
+
+t.ok(depValid({
+  resolved: 'https://registry.npmjs.org/foo/foo-1.2.3.tgz',
+}, 'latest', {}), 'tagged registry version needs remote tarball')
+
+t.notOk(depValid({
+  resolved: 'git+https://registry.npmjs.org/foo/foo-1.2.3.git',
+}, 'latest', {}), 'tagged registry version needs remote tarball, not git')
+
+t.notOk(depValid({}, 'latest', {}),
+  'tagged registry version needs remote tarball resolution')
+
+t.throws(() => depValid({}, { type: 'not a type' }),
+  Error('Unknown dependency type: not a type'))
+
+t.test('invalid tag name', t => {
+  const requestor = { errors: [] }
+  const child = {}
+  const request = '!!@#$%!#@$!'
+  t.notOk(depValid(child, request, requestor))
+  t.match(requestor, {
+    errors: [ { message: 'Invalid tag name "!!@#$%!#@$!"' } ],
+  }, 'parent got an error for their invalid request')
+  t.end()
+})

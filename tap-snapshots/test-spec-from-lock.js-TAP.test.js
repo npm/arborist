783ec99282f2befe15a8b453df0494708ac0c116
isaacs <i@izs.me>
2019-08-05 15:17:06 -0700

    Get a spec from a lockfile entry

---
+++ tap-snapshots/test-spec-from-lock.js-TAP.test.js
@@ -0,0 +1,132 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/spec-from-lock.js TAP > directory symlink 1`] = `
+Object {
+  "escapedName": "x",
+  "fetchSpec": "/Users/isaacs/dev/npm/some/path",
+  "gitCommittish": undefined,
+  "gitRange": undefined,
+  "hosted": undefined,
+  "name": "x",
+  "raw": "x@file:../some/path",
+  "rawSpec": "file:../some/path",
+  "registry": undefined,
+  "saveSpec": "file:../some/path",
+  "scope": undefined,
+  "type": "directory",
+  "where": "/Users/isaacs/dev/npm/arborist",
+}
+`
+
+exports[`test/spec-from-lock.js TAP > file with from, no integrity 1`] = `
+Object {
+  "escapedName": "x",
+  "fetchSpec": "/Users/isaacs/dev/npm/arborist/x-1.2.3.tgz",
+  "gitCommittish": undefined,
+  "gitRange": undefined,
+  "hosted": undefined,
+  "name": "x",
+  "raw": "x@x-1.2.3.tgz",
+  "rawSpec": "x-1.2.3.tgz",
+  "registry": undefined,
+  "saveSpec": "file:x-1.2.3.tgz",
+  "scope": undefined,
+  "type": "file",
+  "where": "/Users/isaacs/dev/npm/arborist",
+}
+`
+
+exports[`test/spec-from-lock.js TAP > file with resolved and from 1`] = `
+Object {
+  "escapedName": "x",
+  "fetchSpec": "/path/to/x-1.2.3.tgz",
+  "gitCommittish": undefined,
+  "gitRange": undefined,
+  "hosted": undefined,
+  "name": "x",
+  "raw": "x@/path/to/x-1.2.3.tgz",
+  "rawSpec": "/path/to/x-1.2.3.tgz",
+  "registry": undefined,
+  "saveSpec": "file:/path/to/x-1.2.3.tgz",
+  "scope": undefined,
+  "type": "file",
+  "where": "/Users/isaacs/dev/npm/arborist",
+}
+`
+
+exports[`test/spec-from-lock.js TAP > file with resolved, no integrity 1`] = `
+Object {
+  "escapedName": "x",
+  "fetchSpec": "/path/to/x-1.2.3.tgz",
+  "gitCommittish": undefined,
+  "gitRange": undefined,
+  "hosted": undefined,
+  "name": "x",
+  "raw": "x@/path/to/x-1.2.3.tgz",
+  "rawSpec": "/path/to/x-1.2.3.tgz",
+  "registry": undefined,
+  "saveSpec": "file:/path/to/x-1.2.3.tgz",
+  "scope": undefined,
+  "type": "file",
+  "where": "/Users/isaacs/dev/npm/arborist",
+}
+`
+
+exports[`test/spec-from-lock.js TAP > version (file) and integrity set 1`] = `
+Object {
+  "escapedName": "x",
+  "fetchSpec": "/Users/isaacs/dev/npm/arborist/foo.tgz",
+  "gitCommittish": undefined,
+  "gitRange": undefined,
+  "hosted": undefined,
+  "name": "x",
+  "raw": "x@foo.tgz",
+  "rawSpec": "foo.tgz",
+  "registry": undefined,
+  "saveSpec": "file:foo.tgz",
+  "scope": undefined,
+  "type": "file",
+  "where": "/Users/isaacs/dev/npm/arborist",
+}
+`
+
+exports[`test/spec-from-lock.js TAP > version and integrity 1`] = `
+Object {
+  "escapedName": "x",
+  "fetchSpec": "1.2.3",
+  "gitCommittish": undefined,
+  "gitRange": undefined,
+  "hosted": undefined,
+  "name": "x",
+  "raw": "x@1.2.3",
+  "rawSpec": "1.2.3",
+  "registry": true,
+  "saveSpec": null,
+  "scope": undefined,
+  "type": "version",
+  "where": undefined,
+}
+`
+
+exports[`test/spec-from-lock.js TAP > version and range, no integrity 1`] = `
+Object {
+  "escapedName": "x",
+  "fetchSpec": "1.2.3",
+  "gitCommittish": undefined,
+  "gitRange": undefined,
+  "hosted": undefined,
+  "name": "x",
+  "raw": "x@1.2.3",
+  "rawSpec": "1.2.3",
+  "registry": true,
+  "saveSpec": null,
+  "scope": undefined,
+  "type": "version",
+  "where": undefined,
+}
+`
---
+++ test/spec-from-lock.js
@@ -0,0 +1,39 @@
+const sfl = require('../lib/spec-from-lock.js')
+const t = require('tap')
+
+t.matchSnapshot(sfl('x', {
+  version: '1.2.3',
+  integrity: 'integral',
+}), 'version and integrity')
+
+t.matchSnapshot(sfl('x', {
+  version: 'foo.tgz',
+  integrity: 'integral',
+}), 'version (file) and integrity set')
+
+t.matchSnapshot(sfl('x', {
+  version: '1.2.3',
+  from: '^1.2.0',
+  shasum: 'deadbeef0cafebad',
+  resolved: 'https://registry.npmjs.org/x/-/x-1.2.3.tgz',
+}), 'version and range, no integrity')
+
+t.matchSnapshot(sfl('x', {
+  version: 'file:x-1.2.3.tgz',
+  from: 'x-1.2.3.tgz',
+}), 'file with from, no integrity')
+
+t.matchSnapshot(sfl('x', {
+  version: 'file:x-1.2.3.tgz',
+  resolved: '/path/to/x-1.2.3.tgz',
+}), 'file with resolved, no integrity')
+
+t.matchSnapshot(sfl('x', {
+  version: 'file:x-1.2.3.tgz',
+  from: 'file:x-1.2.3.tgz',
+  resolved: '/path/to/x-1.2.3.tgz',
+}), 'file with resolved and from')
+
+t.matchSnapshot(sfl('x', {
+  version: 'file:../some/path',
+}), 'directory symlink')

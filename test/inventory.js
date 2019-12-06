0cad4e8871ed808f76566faefd09bd5a01e01f68
isaacs <i@izs.me>
2019-12-05 17:10:22 -0800

    inventory: be resilient against mutated/invalid data

--- test/inventory.js
+++ test/inventory.js
@@ -9,10 +9,14 @@ t.test('basic operations', t => {
   i.add({ location: 'x', name: 'x', 'package': { license: 'MIT' }})
   i.add({ location: 'y', name: 'x', 'package': { license: 'ISC' }})
   i.add({ location: 'z', name: 'z', 'package': { license: 'MIT' }})
+  t.same([...i.query('license')].sort((a,b)=>a.localeCompare(b)),
+    [ 'ISC', 'MIT'])
   t.same([...i.query('license', 'MIT')], [
     { location: 'x', name: 'x', 'package': { license: 'MIT' }},
     { location: 'z', name: 'z', 'package': { license: 'MIT' }},
   ], 'can query by license')
+  t.same(i.query('license', 'blerg'), new Set(),
+    'empty query returns empty set')
   t.same([...i.query('name', 'x')], [
     { location: 'x', name: 'x', 'package': { license: 'MIT' }},
     { location: 'y', name: 'x', 'package': { license: 'ISC' }},
@@ -40,6 +44,18 @@ t.test('basic operations', t => {
   i.delete(y)
   t.equal(i.has(y), false, 'no longer has the y node')
   t.equal(i.get('y'), undefined, 'get returns undefined')
+
+  // no name or license!
+  const z = { location: 'z' }
+  i.add(z)
+  t.equal(i.get('z'), z, 'can add and retrieve node without secondary keys')
+  // oops!  mutated out of band, that's weird, but shouldn't throw anyway
+  // though, of course, this makes the secondary indexes incomplete.
+  // don't do this.  but probably not worth making the package immutable.
+  z.package = { name: 'z' }
+  t.doesNotThrow(() => i.delete(z), 'doesnt try to delete from nonexistent sets')
+  t.equal(i.get('z'), undefined, 'node is not here')
+  t.equal(i.has(z), false, 'i does not have z any more')
 
   t.end()
 })

02b75128a6f990a4a5d0d3c665c1b594da82d5d9
isaacs <i@izs.me>
2019-12-20 22:49:27 -0800

    inventory: add funding to default query keys

--- test/inventory.js
+++ test/inventory.js
@@ -4,35 +4,39 @@ const t = require('tap')
 t.test('basic operations', t => {
   const i = new Inventory()
   t.equal(i.primaryKey, 'location')
-  t.same(i.indexes, ['name', 'license'])
+  t.same(i.indexes, ['name', 'license', 'funding'])
 
-  i.add({ location: 'x', name: 'x', 'package': { license: 'MIT' }})
-  i.add({ location: 'y', name: 'x', 'package': { license: 'ISC' }})
-  i.add({ location: 'z', name: 'z', 'package': { license: 'MIT' }})
+  i.add({ location: 'x', name: 'x', 'package': { license: 'MIT', funding: 'foo' }})
+  i.add({ location: 'y', name: 'x', 'package': { license: 'ISC', funding: { url: 'foo' } }})
+  i.add({ location: 'z', name: 'z', 'package': { license: { type: 'MIT' }, funding: 'bar' }})
+  i.add({ location: 'a', name: 'a', 'package': {}})
 
   t.same(i.filter(node => /[xy]/.test(node.name)), [
     i.get('x'),
     i.get('y'),
   ], 'filter returns an iterable of all matching nodes')
 
-  t.same([...i.query('license')].sort((a,b) => a.localeCompare(b)),
-    [ 'ISC', 'MIT'])
+  t.same([...i.query('license')].sort((a,b) => String(a).localeCompare(String(b))),
+    [ 'ISC', 'MIT', undefined ])
   t.same([...i.query('license', 'MIT')], [
-    { location: 'x', name: 'x', 'package': { license: 'MIT' }},
-    { location: 'z', name: 'z', 'package': { license: 'MIT' }},
+    { location: 'x', name: 'x', 'package': { license: 'MIT', funding: 'foo' }},
+    { location: 'z', name: 'z', 'package': { license: { type: 'MIT' }, funding: 'bar'}},
   ], 'can query by license')
   t.same(i.query('license', 'blerg'), new Set(),
     'empty query returns empty set')
   t.same([...i.query('name', 'x')], [
-    { location: 'x', name: 'x', 'package': { license: 'MIT' }},
-    { location: 'y', name: 'x', 'package': { license: 'ISC' }},
+    { location: 'x', name: 'x', 'package': { license: 'MIT', funding: 'foo'}},
+    { location: 'y', name: 'x', 'package': { license: 'ISC', funding: { url: 'foo' } }},
   ], 'can query by name')
-
-  t.same([...i.query('license')], [ 'MIT', 'ISC' ],
-    'can get the list of license values')
+  t.same([...i.query('funding')].sort((a, b) => String(a).localeCompare(String(b))),
+    ['bar', 'foo', undefined])
+  t.same([...i.query('funding', 'foo')], [
+    { location: 'x', name: 'x', 'package': { license: 'MIT', funding: 'foo' } },
+    { location: 'y', name: 'x', 'package': { license: 'ISC', funding: { url: 'foo' } }},
+  ], 'can query by funding url')
 
   const x = i.get('x')
-  t.same(x, { location: 'x', name: 'x', 'package': { license: 'MIT' }}, 'get by location')
+  t.same(x, { location: 'x', name: 'x', 'package': { license: 'MIT', funding: 'foo' }}, 'get by location')
   i.add(x)
   t.same(i.get('x'), x, 'adding a second time has no effect')
   t.equal(i.has(x), true, 'has a node')
@@ -40,6 +44,9 @@ t.test('basic operations', t => {
   t.same(i.get('x'), { location: 'x', name: 'a', 'package': { license: 'ABC' }},
     'new node at same location overwrites')
   t.equal(i.has(x), false, 'node has been overwritten')
+
+  const a = i.get('a')
+  t.same([...i.query('license', undefined)], [a], 'can query by missing license')
 
   t.throws(() => i.set('a', 'b'), {
     message: 'direct set() not supported, use inventory.add(node)',

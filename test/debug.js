057aec54914f0ecca2a0f205dac611acd1c6fb3a
isaacs <i@izs.me>
2020-12-07 14:21:25 -0800

    debug: add debug.log, run by default in dev folder
    Also, make it possible to disable entirely by setting ARBORIST_DEBUG=0
    in the environment.

--- test/debug.js
+++ test/debug.js
@@ -1,5 +1,6 @@
 const t = require('tap')
 const requireInject = require('require-inject')
+const { resolve } = require('path')
 
 // start from clean slate
 delete process.env.ARBORIST_DEBUG
@@ -7,12 +8,48 @@ delete process.env.NODE_DEBUG
 delete process.env.npm_lifecycle_event
 delete process.env.npm_package_name
 
-t.test('debug does nothing by default', t => {
+const LOGS = []
+console.error = (...msg) => LOGS.push(msg)
+
+t.test('debug does nothing when ARBORIST_DEBUG explicitly 0', t => {
+  process.env.ARBORIST_DEBUG = '0'
   const debug = requireInject('../lib/debug.js', {})
   debug(() => {
     throw new Error('this should not happen')
   })
+  debug.log('hello')
+  t.strictSame(LOGS, [])
+  delete process.env.ARBORIST_DEBUG
   t.end()
+})
+
+t.test('debug does nothing outside arborist folder', t => {
+  const dir = resolve(__dirname)
+  const cwd = process.cwd()
+  t.teardown(() => process.chdir(cwd))
+  process.chdir(dir)
+  const debug = requireInject('../lib/debug.js', {})
+  debug.log('hello')
+  t.strictSame(LOGS, [])
+  debug(() => {
+    throw new Error('this should not happen')
+  })
+  t.end()
+})
+
+t.test('debug runs when cwd is arborist folder', t => {
+  const dir = resolve(__dirname, '..')
+  const cwd = process.cwd()
+  t.teardown(() => process.chdir(cwd))
+  process.chdir(dir)
+  const debug = requireInject('../lib/debug.js', {})
+  t.plan(2)
+  debug(() => {
+    t.pass('called debug function')
+  })
+  debug.log('hello')
+  t.strictSame(LOGS, [['hello']])
+  LOGS.length = 0
 })
 
 t.test('debug runs when NODE_DEBUG contains "arborist"', t => {

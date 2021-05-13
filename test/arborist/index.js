71e962f7a10fd7c56e762efcafbe71629a2c53ed
isaacs <i@izs.me>
2021-05-13 08:45:10 -0700

    Arborist.workspaceNodes and .workspaceDependencySet methods
    This adds a simple way for an Arborist instance to turn a list of
    workspace names into a list of nodes, or a set of all deps (direct and
    transitive) that are relied upon by those workspace nodes.
    
    Note that this returns the *target* of the workspace, not the link in
    the root node_modules folder.
    
    PR-URL: https://github.com/npm/arborist/pull/281
    Credit: @isaacs
    Close: #281
    Reviewed-by: @ruyadorno

--- test/arborist/index.js
+++ test/arborist/index.js
@@ -1,4 +1,8 @@
 const t = require('tap')
+
+const Edge = require('../../lib/edge.js')
+const Link = require('../../lib/link.js')
+
 const Arborist = require('../../lib/arborist/index.js')
 const normalizePath = path => path.replace(/[A-Z]:/, '').replace(/\\/g, '/')
 
@@ -20,3 +24,44 @@ t.equal(c.options.packumentCache, packumentCache, 'passed in a packument cache')
 t.throws(() => {
   new Arborist({ saveType: 'something' })
 }, /saveType/, 'rejects invalid saveType')
+
+t.test('workspace nodes and deps', async t => {
+  const { resolve } = require('path')
+  const fixture = resolve(__dirname, '../fixtures/workspaces-shared-deps-virtual')
+  const arb = new Arborist({ path: fixture })
+  const tree = await arb.loadVirtual()
+  const wsNodes = arb.workspaceNodes(tree, ['b'])
+  t.equal(wsNodes.length, 1, 'got one node')
+  t.equal(wsNodes[0], tree.children.get('b').target, 'got the right node')
+
+  {
+    const wsDepSet = arb.workspaceDependencySet(tree, ['b'])
+    t.equal(wsDepSet.size, 2)
+    t.equal(wsDepSet.has(tree.children.get('b').target), true)
+    t.equal(wsDepSet.has(tree.children.get('abbrev')), true)
+  }
+
+  const wsNode = wsNodes[0]
+  new Edge({
+    from: wsNode,
+    type: 'prod',
+    name: 'xyz',
+    spec: '1.2.3',
+  })
+
+  // move abbrev under the 'a' workspace, and make a link to it
+  tree.children.get('abbrev').parent = tree.children.get('a').target
+  new Link({
+    parent: tree,
+    target: tree.children.get('a').target.children.get('abbrev'),
+  })
+
+  {
+    // verify that xyz is not in the set, but abbrev AND its link both are
+    const wsDepSet = arb.workspaceDependencySet(tree, ['b'])
+    t.equal(wsDepSet.size, 3)
+    t.equal(wsDepSet.has(tree.children.get('b').target), true)
+    t.equal(wsDepSet.has(tree.children.get('abbrev')), true)
+    t.equal(wsDepSet.has(tree.children.get('abbrev').target), true)
+  }
+})

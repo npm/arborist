26c05d50404789efcc0b69031669da931d40841c
isaacs <i@izs.me>
2021-05-17 13:11:25 -0700

    Make audit and audit-fix workspace-aware

--- test/arborist/audit.js
+++ test/arborist/audit.js
@@ -7,7 +7,13 @@ const fixtures = resolve(__dirname, '../fixtures')
 
 const fixture = (t, p) => require(fixtures + '/reify-cases/' + p)(t)
 
-const {start, stop, registry, auditResponse} = require('../fixtures/registry-mocks/server.js')
+const {
+  start,
+  stop,
+  registry,
+  auditResponse,
+  advisoryBulkResponse,
+} = require('../fixtures/registry-mocks/server.js')
 const cache = t.testdir()
 
 t.before(start)
@@ -18,6 +24,9 @@ const {
   printTree,
 } = require('../utils.js')
 
+const newArb = (path, options = {}) =>
+  new Arborist({ path, cache, registry, ...options })
+
 const cwd = normalizePath(process.cwd())
 t.cleanSnapshot = s => s.split(cwd).join('{CWD}')
   .split(registry).join('https://registry.npmjs.org/')
@@ -26,11 +35,7 @@ t.test('audit finds the bad deps', async t => {
   const path = resolve(fixtures, 'deprecated-dep')
   t.teardown(auditResponse(resolve(fixtures, 'audit-nyc-mkdirp/audit.json')))
 
-  const arb = new Arborist({
-    cache,
-    path,
-    registry,
-  })
+  const arb = newArb(path)
 
   const report = await arb.audit()
   t.equal(report.topVulns.size, 0)
@@ -40,17 +45,65 @@ t.test('audit finds the bad deps', async t => {
 t.test('audit fix reifies out the bad deps', async t => {
   const path = fixture(t, 'deprecated-dep')
   t.teardown(auditResponse(resolve(fixtures, 'audit-nyc-mkdirp/audit.json')))
-  const arb = new Arborist({
-    cache,
-    path,
-    registry,
-  })
+  const arb = newArb(path)
   const tree = printTree(await arb.audit({fix: true}))
   t.matchSnapshot(tree, 'reified out the bad mkdirp and minimist')
 })
 
 t.test('audit does not do globals', t =>
-  t.rejects(new Arborist({ cache, path: '.', global: true }).audit(), {
+  t.rejects(newArb('.', { global: true }).audit(), {
     message: '`npm audit` does not support testing globals',
     code: 'EAUDITGLOBAL',
   }))
+
+t.test('audit in a workspace', async t => {
+  const src = resolve(fixtures, 'audit-nyc-mkdirp')
+  const auditFile = resolve(src, 'advisory-bulk.json')
+  t.teardown(advisoryBulkResponse(auditFile))
+
+  const path = t.testdir({
+    'package.json': JSON.stringify({
+      workspaces: ['packages/*'],
+      dependencies: {
+        mkdirp: '1',
+      },
+    }),
+    packages: {
+      a: {
+        'package.json': JSON.stringify({
+          name: 'a',
+          version: '1.2.3',
+          dependencies: {
+            mkdirp: '0',
+          },
+        }),
+      },
+      b: {
+        'package.json': JSON.stringify({
+          name: 'b',
+          version: '1.2.3',
+          dependencies: {
+            mkdirp: '0',
+          },
+        }),
+      },
+    },
+  })
+
+  // reify it without auditing so that we can put the "bad" versions
+  // in place and save a lockfile reflecting this.
+  await newArb(path, { audit: false }).reify()
+  const bad = 'mkdirp@0.5.0'
+  await newArb(path, { audit: false, workspaces: ['a'] }).reify({ add: [bad] })
+  await newArb(path, { audit: false, workspaces: ['b'] }).reify({ add: [bad] })
+
+  const auditReport = await newArb(path, { workspaces: ['a'] }).audit()
+  t.equal(auditReport.get('mkdirp').nodes.size, 1)
+  t.strictSame(auditReport.toJSON().vulnerabilities.mkdirp.nodes, ['packages/a/node_modules/mkdirp'])
+  t.equal(auditReport.get('minimist').nodes.size, 1)
+  t.strictSame(auditReport.toJSON().vulnerabilities.minimist.nodes, ['packages/a/node_modules/minimist'])
+
+  const fixed = await newArb(path, { workspaces: ['b'] }).audit({ fix: true })
+  t.equal(fixed.children.get('a').target.children.get('mkdirp').version, '0.5.0', 'did not fix a')
+  t.equal(fixed.children.get('b').target.children.get('mkdirp').version, '0.5.5', 'did fix b')
+})

bafacfa3d54c4d0da7849877d6b331b3321d40e8
Vincent Bailly <vibailly@microsoft.com>
2021-11-16 19:08:11 +0100

    create basic end to end test for isolated mode

---
+++ test/isolated-mode.js
@@ -0,0 +1,24 @@
+const tap = require('tap')
+const fs = require('fs')
+const path = require('path')
+const Arborist = require('../lib/arborist')
+
+tap.test('most simple happy scenario', async t => {
+  const package = { name: 'foo', dependencies: { 'which': '*' } }
+
+  const cwd = t.testdir({
+    'package.json': JSON.stringify(package)
+  })
+
+  const arborist = new Arborist({ path: cwd })
+  await arborist.reify({ isolated: true })
+
+  // Only direct dependencies are accessible by the node-module resolution algorithm
+  t.throws(() => require.resolve('isexe', { paths: [ cwd ] }), 'repo should be able to require direct dependencies')
+  t.doesNotThrow(() => require.resolve('which', { paths: [ cwd ] }), 'repo should not be able to require transitive dependencies')
+
+  // Transitive dependencies are accessible from their parents
+  const whichPath = path.dirname(require.resolve("which", { paths: [ cwd ] }))
+  t.doesNotThrow(() => require.resolve('isexe', { paths: [ whichPath ] }), 'external dependencies should be able to require their own dependencies')
+
+})

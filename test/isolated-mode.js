1fabcc559d9b5b11caf5fb650395de4e1832bf9a
Vincent Bailly <vibailly@microsoft.com>
2021-12-15 13:08:18 +0100

    test peer deps on parents

--- test/isolated-mode.js
+++ test/isolated-mode.js
@@ -795,7 +795,7 @@ tap.test('circular dependencies', async t => {
   rule7.apply(t, dir, resolved, asserted)
 })
 
-tap.only('circular peer dependencies', async t => {
+tap.test('circular peer dependencies', async t => {
 
   // Input of arborist
   const graph = {
@@ -837,6 +837,44 @@ tap.only('circular peer dependencies', async t => {
   rule7.apply(t, dir, resolved, asserted)
 })
 
+tap.test('peer dependency on parent', async t => {
+
+  // Input of arborist
+  const graph = {
+    registry: [
+      { name: 'cat', version: '1.0.0', dependencies: { bar: '*' } },
+      { name: 'bar', version: '1.0.0', peerDependencies: { cat: '*' } }
+    ] ,
+    root: {
+      name: 'foo', version: '1.2.3', dependencies: { cat: '1.0.0' }
+    }
+  }
+
+  // expected output
+  const resolved = {
+    'foo@1.2.3 (root)': {
+      'cat@1.0.0': {
+        'bar@1.0.0': '(back 1)' 
+      }
+    }
+  }
+
+  const { dir, registry } = await getRepo(graph)
+
+  // Note that we override this cache to prevent interference from other tests
+  const cache = fs.mkdtempSync(`${os.tmpdir}/test-`)
+  const arborist = new Arborist({ path: dir, registry, packumentCache: new Map(), cache  })
+  await arborist.reify({ isolated: true })
+
+  const asserted = new Set()
+  rule1.apply(t, dir, resolved, asserted)
+  rule2.apply(t, dir, resolved, asserted)
+  rule3.apply(t, dir, resolved, asserted)
+  rule4.apply(t, dir, resolved, asserted)
+  rule5.apply(t, dir, resolved, asserted)
+  rule6.apply(t, dir, resolved, asserted)
+  rule7.apply(t, dir, resolved, asserted)
+})
 function setupRequire(cwd) {
   return function requireChain(...chain) {
     return chain.reduce((path, name) => {
@@ -954,7 +992,6 @@ function parseGraphRecursive(key, deps) {
 
 /*
   * TO TEST:
-  * - peer dependencies on the parent
   * - scoped packages
   * - optional peer dependency
   * - virtual packages
